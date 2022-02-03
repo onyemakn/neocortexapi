5560315c7a324b93a7ef89426b3fa4aeb9b65854
Kizito Onyema <onyemakizito@outlook.com>
2022-02-03 20:17:56 +0100

    Project Contribution

--- source/HtmPersistence/UnitTest1.cs
+++ source/HtmPersistence/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace HtmPersistence
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SerializeSparseObjectMatrix()
         {
             // Create SParse BinarySparseMatrix
             // either by dicrect creation or running experiment
@@ -31,8 +31,46 @@ namespace HtmPersistence
             {
                 var json = sr.ReadToEnd();
                 matrixNew = JsonConvert.DeserializeObject<SparseObjectMatrix<int[]>>(json);
-                Assert.AreEqual(matrix, matrixNew);
+
+                var dimMatrix = matrix.GetDimensions();
+                var dimMatrixNew = matrixNew.GetDimensions();
+                Assert.AreEqual(dimMatrix[0], dimMatrixNew[0]);
+                Assert.AreEqual(dimMatrix[1], dimMatrixNew[1]);
+                Assert.AreEqual(matrix.ModuleTopology.IsMajorOrdering, matrixNew.ModuleTopology.IsMajorOrdering);
             }
         }
+
+        [TestMethod]
+        public void SerializeAbstractFlatMatrix()
+        {
+            // Create SParse BinarySparseMatrix
+            // either by dicrect creation or running experiment
+            int[] dimensions = { 10, 10 };
+
+            //IDistributedDictionary<int, int[]> dict = new();
+
+            SparseObjectMatrix<int[]> matrix = new(dimensions, false);
+
+            // Serialize 
+            using (StreamWriter sw = new StreamWriter("ser.txt"))
+            {
+                var json = JsonConvert.SerializeObject(matrix);
+                sw.WriteLine(json.ToString());
+            }
+            // Deserizlize
+            SparseObjectMatrix<int[]> matrixNew = new();
+            using (StreamReader sr = new StreamReader("ser.txt"))
+            {
+                var json = sr.ReadToEnd();
+                matrixNew = JsonConvert.DeserializeObject<SparseObjectMatrix<int[]>>(json);
+
+                var dimMatrix = matrix.GetDimensions();
+                var dimMatrixNew = matrixNew.GetDimensions();
+                Assert.AreEqual(dimMatrix[0], dimMatrixNew[0]);
+                Assert.AreEqual(dimMatrix[1], dimMatrixNew[1]);
+                Assert.AreEqual(matrix.ModuleTopology.IsMajorOrdering, matrixNew.ModuleTopology.IsMajorOrdering);
+            }
+        }
+
     }
 }
