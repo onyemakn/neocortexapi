3c805c239cafd0e4851396cf370bec610f03f4a8
Kizito Onyema <onyemakizito@outlook.com>
2022-02-03 18:54:34 +0100

    Unit Test code

--- source/HtmPersistence/UnitTest1.cs
+++ source/HtmPersistence/UnitTest1.cs
@@ -1,4 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeoCortexApi.Entities;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace HtmPersistence
 {
@@ -8,6 +11,28 @@ namespace HtmPersistence
         [TestMethod]
         public void TestMethod1()
         {
+            // Create SParse BinarySparseMatrix
+            // either by dicrect creation or running experiment
+            int[] dimensions = { 10, 10 };
+
+            //IDistributedDictionary<int, int[]> dict = new();
+
+            SparseObjectMatrix<int[]> matrix = new(dimensions, false);
+
+            // Serialize 
+            using (StreamWriter sw = new StreamWriter("ser.txt"))
+            {
+                var json = JsonConvert.SerializeObject(matrix);
+                sw.WriteLine(json.ToString());
+            }
+            // Deserizlize
+            SparseObjectMatrix<int[]> matrixNew = new();
+            using (StreamReader sr = new StreamReader("ser.txt"))
+            {
+                var json = sr.ReadToEnd();
+                matrixNew = JsonConvert.DeserializeObject<SparseObjectMatrix<int[]>>(json);
+                Assert.AreEqual(matrix, matrixNew);
+            }
         }
     }
 }
